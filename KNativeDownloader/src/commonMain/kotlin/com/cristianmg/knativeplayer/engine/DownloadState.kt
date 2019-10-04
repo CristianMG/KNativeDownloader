1eb01476012c511bf899467dee9b4d67287fc1ee
Cristian <cristian.menarguez.gonzalez@gmail.com>
2019-10-04 13:34:59 +0200

    Added download engine and delete old license
    Added some functionally for first download

---
+++ KNativeDownloader/src/commonMain/kotlin/com/cristianmg/knativeplayer/engine/DownloadState.kt
@@ -0,0 +1,9 @@
+package com.cristianmg.knativeplayer.engine
+
+/**
+ * This class represents the download's state for each moment
+ */
+data class DownloadState(
+        val percentProgress: Int,
+        val speedDownload: Int
+)
---
+++ KNativeDownloader/src/iosMain/kotlin/com/cristianmg/knativeplayer/engine/DownloaderEngine.kt
@@ -0,0 +1,5 @@
+package com.cristianmg.knativeplayer.engine
+
+actual fun downloadItem(downloadState: (DownloadState) -> Unit, url: String) {
+
+}
